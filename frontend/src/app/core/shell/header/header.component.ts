6cd8cc24e9cdb2bd6fc38bade01082d26fe4d33b
Puentes <joseluispuentes@hotmail.com>
2021-09-10 21:59:18 +0200

    creando proyecto angular

---
+++ frontend/src/app/core/shell/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { faDotCircle } from '@fortawesome/free-regular-svg-icons';
+import { faBed, faBone, faDog, faUser, faUsersCog } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  faUser =faUser;
+  faDog = faDog;
+  faBed = faBed;
+  faBone = faBone;
+  faUsersCog = faUsersCog;
+  faDotCircle = faDotCircle;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+
+
+
+}
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  appName: 'Sicenad PROD',
+  production: true
+};
