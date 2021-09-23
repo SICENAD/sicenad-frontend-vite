b3d6ea9adbb40dffdb3174910503221d114f12f2
Jose <joseluispuentes@hotmail.com>
2021-09-23 10:09:51 +0200

    Backend CRUD CENAD_CMT Fix #4

--- src/main/java/es/mde/entidades/Cenad.java
+++ src/main/java/es/mde/entidades/Cenad.java
@@ -1,13 +1,16 @@
 package es.mde.entidades;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,8 +23,21 @@ public class Cenad {
 	@Column(unique=true)
 	private Long id;
 	private String nombre;
-	private String provincia;
-		
+	private int provincia;
+	private String descripcion;
+	private String direccion;
+	private String tfno;
+	private String email;
+	private String escudo;
+	@OneToMany(cascade = CascadeType.ALL, targetEntity = Categoria.class, mappedBy = "cenad")
+	private Collection<Categoria> categorias = new ArrayList<>();
+	@OneToMany(cascade = CascadeType.ALL, targetEntity = Cartografia.class, mappedBy = "cenad")
+	private Collection<Cartografia> cartografias = new ArrayList<>();
+	@OneToMany(cascade = CascadeType.ALL, targetEntity = UsuarioGestor.class, mappedBy = "cenad")
+	private Collection<UsuarioGestor> usuariosGestores = new ArrayList<>();
+	@OneToOne(cascade = CascadeType.ALL, targetEntity = UsuarioAdministrador.class, mappedBy = "cenad")
+	private UsuarioAdministrador usuarioAdministrador;
+	
 	public Cenad() {
 		super();
 	}
@@ -42,24 +58,104 @@ public class Cenad {
 		this.nombre = nombre;
 	}
 
-	public String getProvincia() {
+	public int getProvincia() {
 		return provincia;
 	}
 
-	public void setProvincia(String provincia) {
+	public void setProvincia(int provincia) {
 		this.provincia = provincia;
 	}
-	
-	@OneToMany(targetEntity = Recurso.class)
-	public Collection<Recurso> getRecursos() {
-		return getRecursos();
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTfno() {
+		return tfno;
+	}
+
+	public void setTfno(String tfno) {
+		this.tfno = tfno;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEscudo() {
+		return escudo;
+	}
+
+	public void setEscudo(String escudo) {
+		this.escudo = escudo;
+	}
+
+	public Collection<Categoria> getCategorias() {
+		return categorias;
 	}
 	
-
-
+	public void setCategorias(Collection<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+	
+	public Collection<Cartografia> getCartografias() {
+		return cartografias;
+	}
+	
+	public void setCartografias(Collection<Cartografia> cartografias) {
+		this.cartografias = cartografias;
+	}
+	
+	public Collection<UsuarioGestor> getUsuariosGestores() {
+		return usuariosGestores;
+	}
+	
+	public void setUsuariosGestores(Collection<UsuarioGestor> usuarios) {
+		this.usuariosGestores = usuarios;
+	}
+	
+	public UsuarioAdministrador getUsuarioAdministrador() {
+		return usuarioAdministrador;
+	}
+	
+	public void setUsuarioAdministrador(UsuarioAdministrador usuarioAdministrador) {
+		this.usuarioAdministrador = usuarioAdministrador;
+	}
+	
 	// Establece la relacion en los dos sentidos
-	public void addRecurso(Recurso recurso) {
-		getRecursos().add(recurso);
-		recurso.setCenad(this);
+	public void addCategoria(Categoria categoria) {
+		getCategorias().add(categoria);
+		categoria.setCenad(this);
+	}
+	
+	public void addCartografia(Cartografia cartografia) {
+		getCartografias().add(cartografia);
+		cartografia.setCenad(this);
+	}
+	
+	public void addUsuariosGestores(UsuarioGestor usuario) {
+		getUsuariosGestores().add(usuario);
+		usuario.setCenad(this);
+	}
+	
+	@Override
+	public String toString() {
+		return "CENAD/CMT " + nombre + "(" + provincia + ")";
 	}
 }
